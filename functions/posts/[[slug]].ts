2636a379bfb026fb68ebd21e7af48c38088cb3b0
okonomi <okonomi@oknm.jp>
2022-10-09 17:35:59 +0900

    switch to ts

--- functions/posts/[[slug]].ts
+++ functions/posts/[[slug]].ts
@@ -1,4 +1,4 @@
-export async function onRequest({ request, next }) {
+export const onRequest: PagesFunction = async ({ request, next }) => {
   const url = request.url
   if (!url.endsWith('/')) {
     const res = await fetch(`${url}/`)
