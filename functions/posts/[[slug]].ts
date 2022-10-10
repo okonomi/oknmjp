44340cd96e3d64d17cda3a9cc0d6f43658e3f1de
okonomi <okonomi@oknm.jp>
2022-10-10 23:42:33 +0900

    respect original response headers

--- functions/posts/[[slug]].ts
+++ functions/posts/[[slug]].ts
@@ -2,7 +2,9 @@ export const onRequest: PagesFunction = async ({ request, next }) => {
   const url = request.url
   if (!url.endsWith('/')) {
     const res = await fetch(`${url}/`)
-    return new Response(res.body);
+    return new Response(res.body, {
+      headers: res.headers
+    });
   }
 
   return await next();
