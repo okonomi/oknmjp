7083c781ac33146605c649f2b14b9c1ff2c6a617
okonomi <okonomi@oknm.jp>
2022-11-08 00:25:07 +0900

    check response status

--- functions/posts/[[slug]].ts
+++ functions/posts/[[slug]].ts
@@ -2,9 +2,11 @@ export const onRequest: PagesFunction = async ({ request, next }) => {
   const url = request.url
   if (!url.endsWith('/')) {
     const res = await fetch(`${url}/`)
-    return new Response(res.body, {
-      headers: res.headers
-    });
+    if (res.ok) {
+      return new Response(res.body, {
+        headers: res.headers
+      });
+    }
   }
 
   return await next();
