0d615f9e4d9427f128f1d9eb72836c8812f09bef
okonomi <okonomi@oknm.jp>
2022-11-08 22:17:28 +0900

    redirect old url to new url

--- functions/posts/[[slug]].ts
+++ functions/posts/[[slug]].ts
@@ -1,5 +1,10 @@
 export const onRequest: PagesFunction = async ({ request, next }) => {
   const url = request.url
+
+  if (url == 'https://oknm.jp/posts/2022-11-06-google-cloud-sdk-release-notes-feed') {
+    return Response.redirect('https://oknm.jp/posts/2022-11-06-google-cloud-cli-release-notes-feed', 301)
+  }
+
   if (!url.endsWith('/')) {
     const res = await fetch(`${url}/`)
     if (res.ok) {
