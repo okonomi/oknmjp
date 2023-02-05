b8015e16b2c4bb7927a89dc16368380146fd0ee2
okonomi <okonomi@oknm.jp>
2023-02-05 19:27:45 +0900

    migrate to content collection

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,10 +1,15 @@
 import rss from '@astrojs/rss';
+import { getCollection } from "astro:content"
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
-export const get = () =>
-  rss({
+export const get = () => {
+  const posts = getCollection("posts")
+  return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: import.meta.glob('./posts/**/*.{md,mdx}'),
+    items: posts.map(post => ({
+      title: post.data.title,
+    }))
   });
+}
