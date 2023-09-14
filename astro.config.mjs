ca41d2a78cdfdcbde932e84b92de971165855d12
okonomi <okonomi@oknm.jp>
2023-09-14 22:47:01 +0900

    npx prettier -w "./**/*.mjs"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 import remarkBreaks from "remark-breaks"
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx"
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oknm.jp',
-  integrations: [
-    mdx(),
-    sitemap()
-  ],
+  site: "https://oknm.jp",
+  integrations: [mdx(), sitemap()],
   markdown: {
-    remarkPlugins: [
-      remarkBreaks
-    ]
-  }
-});
+    remarkPlugins: [remarkBreaks],
+  },
+})
