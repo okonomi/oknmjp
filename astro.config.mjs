f19879a179711d3f129d53011a5d71882ff071f5
okonomi <okonomi@oknm.jp>
2025-02-23 16:21:32 +0900

    feat: pnpm astro add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,18 @@ import mdx from "@astrojs/mdx"
 
 import sitemap from "@astrojs/sitemap"
 
+import tailwindcss from "@tailwindcss/vite"
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://oknm.jp",
   integrations: [mdx(), sitemap()],
+
   markdown: {
     remarkPlugins: [remarkBreaks],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 })
