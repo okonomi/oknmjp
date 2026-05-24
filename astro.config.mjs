029e7e779c9005daab1da72a37b0861f1820f835
okonomi <okonomi@oknm.jp>
2026-05-24 22:11:11 +0900

    chore: configure Workers Static Assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config"
+import { defineConfig, passthroughImageService } from "astro/config"
 import remarkBreaks from "remark-breaks"
 import remarkHeadingShift from "remark-heading-shift"
 import mdx from "@astrojs/mdx"
@@ -10,10 +10,16 @@ import tailwindcss from "@tailwindcss/vite"
 // https://astro.build/config
 export default defineConfig({
   site: "https://oknm.jp",
+  trailingSlash: "never",
   integrations: [mdx(), sitemap()],
 
   markdown: {
     remarkPlugins: [remarkBreaks, [remarkHeadingShift, 1]],
+  },
+
+  image: {
+    domains: ["res.cloudinary.com"],
+    service: passthroughImageService(),
   },
 
   vite: {
